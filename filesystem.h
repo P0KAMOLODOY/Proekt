f5c4456a2b75830d1614c520302767445e163902
P0KAMOLODOY <denismokeev83139@gmail.com>
2025-05-16 11:41:38 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -1,1 +1,10 @@
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
 
+#include <stdio.h>
+
+FILE* fs_open_or_create(const char* filename);
+char* fs_view_file(const char* filename);
+int fs_delete_file(const char* filename);
+
+#endif
