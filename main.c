0ea2d70731f64b1a990bb551e4c59a79aa9442ab
P0KAMOLODOY <denismokeev83139@gmail.com>
2025-05-16 11:42:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include "filesystem.h"
 
+int main() {
+    
+    FILE* file = fs_open_or_create("test.txt");
+    if (file) {
+        fprintf(file, "Hello, FileSystem!");
+        fclose(file); 
+        
+        char* content = fs_view_file("test.txt");
+        printf("Содержимое: %s\n", content);
+        
+        fs_delete_file("test.txt");
+    }
+    return 0;
+}
