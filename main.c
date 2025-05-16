2f874034366ae6ed9aca727ef82e1bd46b3b53d1
P0KAMOLODOY <denismokeev83139@gmail.com>
2025-05-16 10:21:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,18 +2,15 @@
 #include "filesystem.h"
 
 int main() {
-    // Тест работы файловой системы
+    
     FILE* file = fs_open_or_create("test.txt");
     if (file) {
-        // Записываем что-то в файл (если нужно)
         fprintf(file, "Hello, FileSystem!");
-        fclose(file);  // Не забываем закрыть файл
+        fclose(file); 
         
-        // Читаем содержимое
         char* content = fs_view_file("test.txt");
         printf("Содержимое: %s\n", content);
         
-        // Удаляем файл
         fs_delete_file("test.txt");
     }
     return 0;
